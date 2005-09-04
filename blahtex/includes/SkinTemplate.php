9ff445d52cff8684daca330216263f0608aa9081
jitseniesen <jitseniesen>
2005-09-04 13:29:16 +0000

    Remove XSL style sheet declaration.

--- blahtex/includes/SkinTemplate.php
+++ blahtex/includes/SkinTemplate.php
@@ -243,25 +243,25 @@ class SkinTemplate extends Skin {
 		  $tpl->set( 'mimetype', 'text/xml' );
 		  $tpl->set( 'doctype', '-//W3C//DTD XHTML 1.1 plus MathML 2.0//EN' );
 		  $tpl->set( 'dtd', 'http://www.w3.org/Math/DTD/mathml2/xhtml-math11-f.dtd' );
-		  $tpl->set( 'xmlheaders', "<?xml version=\"1.0\"?>\n<?xml-stylesheet type=\"text/xsl\" href=\"http://www.w3.org/Math/XSL/mathml.xsl\"?>\n" );
+		  $tpl->set( 'xmlheaders', "<?xml version=\"1.0\"?>\n" );
 		  break;
 		case MW_MATH_MATHML_AX:
 		  $tpl->set( 'mimetype', 'application/xml' );
 		  $tpl->set( 'doctype', '-//W3C//DTD XHTML 1.1 plus MathML 2.0//EN' );
 		  $tpl->set( 'dtd', 'http://www.w3.org/Math/DTD/mathml2/xhtml-math11-f.dtd' );
-		  $tpl->set( 'xmlheaders', "<?xml version=\"1.0\"?>\n<?xml-stylesheet type=\"text/xsl\" href=\"http://www.w3.org/Math/XSL/mathml.xsl\"?>\n" );
+		  $tpl->set( 'xmlheaders', "<?xml version=\"1.0\"?>\n" );
 		  break;
 		case MW_MATH_MATHML_AXX:
 		  $tpl->set( 'mimetype', 'application/xhtml+xml' );
 		  $tpl->set( 'doctype', '-//W3C//DTD XHTML 1.1 plus MathML 2.0//EN' );
 		  $tpl->set( 'dtd', 'http://www.w3.org/Math/DTD/mathml2/xhtml-math11-f.dtd' );
-		  $tpl->set( 'xmlheaders', "<?xml version=\"1.0\"?>\n<?xml-stylesheet type=\"text/xsl\" href=\"http://www.w3.org/Math/XSL/mathml.xsl\"?>\n" );
+		  $tpl->set( 'xmlheaders', "<?xml version=\"1.0\"?>\n" );
 		  break;
 		case MW_MATH_MATHML_MIX:
 		  $tpl->set( 'mimetype', 'text/html' );
 		  $tpl->set( 'doctype', '-//W3C//DTD XHTML 1.1 plus MathML 2.0//EN' );
 		  $tpl->set( 'dtd', 'http://www.w3.org/Math/DTD/mathml2/xhtml-math11-f.dtd' );
-		  $tpl->set( 'xmlheaders', "<?xml version=\"1.0\"?>\n<?xml-stylesheet type=\"text/xsl\" href=\"http://www.w3.org/Math/XSL/mathml.xsl\"?>\n" );
+		  $tpl->set( 'xmlheaders', "<?xml version=\"1.0\"?>\n" );
 		  break;
 		}
 
