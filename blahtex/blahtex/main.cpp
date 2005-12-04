7e7e1770a08d08c7fc1d6f7c71f5c972ccf6ed09
jitseniesen <jitseniesen>
2005-12-04 20:12:22 +0000

    #include "errno.h"

--- blahtex/blahtex/main.cpp
+++ blahtex/blahtex/main.cpp
@@ -18,6 +18,7 @@
 // Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 #include "blahtex.h"
+#include "errno.h"
 #include "UnicodeConverter.h"
 #include "md5Wrapper.h"
 #include <iostream>
