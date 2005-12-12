2a126d8f652011d61f0c27bd8d5f18340f13a7b9
jitseniesen <jitseniesen>
2005-12-12 23:02:36 +0000

    Bump blahtex version to 0.3.3

--- blahtex/includes/DefaultSettings.php
+++ blahtex/includes/DefaultSettings.php
@@ -28,7 +28,7 @@ require_once( 'includes/SiteConfiguration.php' );
 $wgConf = new SiteConfiguration;
 
 /** MediaWiki version number */
-$wgVersion			= '1.5.3 + blahtex 0.3.2';
+$wgVersion			= '1.5.3 + blahtex 0.3.3';
 
 /** Name of the site. It must be changed in LocalSettings.php */
 $wgSitename         = 'MediaWiki';
